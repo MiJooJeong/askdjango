3d540fee278856117c25b3bd207e7988152e7e56
MiJooJeong <uuuio1224@gmail.com>
2018-08-08 18:38:48 +0900

    dojo.views.mysum에서 다수 숫자를 받도록 개선

--- dojo/views.py
+++ dojo/views.py
@@ -5,5 +5,6 @@ from django.shortcuts import render
 # Create your views here.
 def mysum(request, numbers):
     # request: HttpRequest
-    result = sum(map(int, numbers.split('/')))
+    # result = sum(map(int, numbers.split('/')))
+    result = sum(map(lambda s:int(s or 0), numbers.split('/')))
     return HttpResponse(result)
