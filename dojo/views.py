bbe46cf0aa436295b83e3b071280edc4707ca29a
MiJooJeong <uuuio1224@gmail.com>
2018-08-09 17:11:27 +0900

    함수 기반 뷰(FBV)의 4가지 패턴

--- dojo/views.py
+++ dojo/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 
 
@@ -6,9 +8,49 @@ from django.shortcuts import render
 def mysum(request, numbers):
     # request: HttpRequest
     # result = sum(map(int, numbers.split('/')))
-    result = sum(map(lambda s:int(s or 0), numbers.split('/')))
+    result = sum(map(lambda s: int(s or 0), numbers.split('/')))
     return HttpResponse(result)
 
 
 def hello(request, name, age):
     return HttpResponse('안녕하세요, {}. {}살이시네요'.format(name, age))
+
+
+def post_list1(request):
+    'FBV: 직접 문자열로 HTML 형식 응답하기'
+
+    name = '공유'
+    return HttpResponse('''
+        <h1>AskDjango</h1>
+        <p>{name}</p>
+        <p>여러분의 파이썬&장고 페이스메이커가 되겠습니다.</p>
+    '''.format(name=name))
+
+
+def post_list2(request):
+    'FBV: 템플릿을 통해 HTML 형식 응답하기'
+
+    name = '공유'
+    return render(request, "dojo/post_list.html", {'name': name})
+
+
+def post_list3(request):
+    'FBV: JSON 형식 응답하기'
+
+    return JsonResponse({
+        'message': '안녕, 파이썬&장고',
+        'items': ['파이썬', '장고', 'Celery', 'Azure', 'AWS'],
+    }, json_dumps_params={'ensure_ascii': False})
+
+
+def excel_download(request):
+    'FBV: 엑셀 다운로드 응답하기'
+
+    # filepath = '/Users/joo/git/study/ask/django/askdjango/180607_SF_HLA.xls'
+    filepath = os.path.join(settings.BASE_DIR, '180607_SF_HLA.xls')
+    filename = os.path.basename(filepath)
+
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
