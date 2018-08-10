91fced9f67eb132bd515d6799a72eaaea9639c48
MiJooJeong <uuuio1224@gmail.com>
2018-08-10 12:21:04 +0900

    blog.models.Post 모델 생성/마이그레이션/admin 등록

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name='제목',
+        help_text='포스팅 제목을 입력해주세요. 최대 100자 내외.')    # 길이 제한이 있는 문자열
+    content = models.TextField(verbose_name='내용')                # 길이 제한이 없는 문자열
+    created_at = models.DateTimeField(auto_now_add=True)    # 최초 저장될 때 일시를 자동 저장해주는 옵션
+    updated_at = models.DateTimeField(auto_now=True)        # 갱신될때마다 일시 저장해주는 옵션
