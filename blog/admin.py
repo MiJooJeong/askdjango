25e556f22753101d0b7bb515590eb78bce99d3d3
MiJooJeong <uuuio1224@gmail.com>
2018-08-14 14:23:31 +0900

    blog.models.Comment 모델 생성/admin등록

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.utils.safestring import mark_safe
-from .models import Post
+from .models import Post, Comments
 
 
 # 등록법 1
@@ -38,3 +38,8 @@ class PostAdmin(admin.ModelAdmin):
         self.message_user(request, '{}건의 포스팅을 Draft 상태로 변경'.format(updated_count))  # django message framework 활용
 
     make_draft.short_description = '지정 포스팅을 Draft 상태로 변경합니다.'
+
+
+@admin.register(Comments)
+class CommentAdmin(admin.ModelAdmin):
+    pass
