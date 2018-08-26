5aeb039623bbc5fadf5e231d9f5a646c364c1ebb
MiJooJeong <uuuio1224@gmail.com>
2018-08-26 17:45:29 +0900

    blog.models.Post의 status field 값 수정

--- blog/models.py
+++ blog/models.py
@@ -25,7 +25,7 @@ class Post(models.Model):
     lnglat = models.CharField(max_length=50, blank=True,
                               validators=[lnglat_validator],
                               help_text='경도/위도 포맷으로 입력')
-    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default='d')
     tag_set = models.ManyToManyField('Tag', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)  # 최초 저장될 때 일시를 자동 저장해주는 옵션
     updated_at = models.DateTimeField(auto_now=True)  # 갱신될때마다 일시 저장해주는 옵션
