a6fe040b1a0a8b4f9580d8d65778e28af0c5bfb4
MiJooJeong <uuuio1224@gmail.com>
2018-08-31 16:37:34 +0900

    dojo.models.Post 모델에 user_agent 필드 추가

--- dojo/forms.py
+++ dojo/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .models import Post
+from .models import GameUser
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         # fields = '__all__'
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'user_agent']
+        widgets = {
+            'user_agent': forms.HiddenInput,
+        }
 
     '''
     def save(self, commit=True):
@@ -15,3 +19,13 @@ class PostForm(forms.ModelForm):
             post.save()
         return post
     '''
+
+
+class GameUserSignupForm(forms.ModelForm):
+    class Meta:
+        model = GameUser
+        fields = ['server_name', 'username']
+
+    def clean_username(self):
+        # 값 변환은 clean 함수에서만 가능. validator는 지원하지 않음.
+        return self.cleaned_data.get('username', '').strip()
