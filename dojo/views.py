2a7fce9e1d399bbf26b9a2cec844dbcf942b7d23
MiJooJeong <uuuio1224@gmail.com>
2018-09-13 16:42:16 +0900

    CBV 샘플 - STEP 4. CBV 구현

--- dojo/views.py
+++ dojo/views.py
@@ -34,35 +34,39 @@ from .models import Post
 # post_detail = generate_view_fn(Post)
 
 
-# STEP 3. CBV 형태로 컨셉만 구현한 버전
-class DetailView(object):
-    """
-    이전 FBV를 CBV 버전으로 컨셉만 간단히 구현. 같은 동작은 수행
-    """
+# # STEP 3. CBV 형태로 컨셉만 구현한 버전
+# class DetailView(object):
+#     """
+#     이전 FBV를 CBV 버전으로 컨셉만 간단히 구현. 같은 동작은 수행
+#     """
+#
+#     def __init__(self, model):
+#         self.model = model
+#
+#     def get_object(self, *args, **kwargs):
+#         return get_object_or_404(self.model, id=kwargs['id'])
+#
+#     def get_template_name(self):
+#         return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
+#
+#     def dispatch(self, request, *args, **kwargs):
+#         return render(request, self.get_template_name(), {
+#             self.model._meta.model_name: self.get_object(*args, **kwargs),
+#         })
+#
+#     @classmethod
+#     def as_view(cls, model):
+#         def view(request, *args, **kwargs):
+#             self = cls(model)
+#             return self.dispatch(request, *args, **kwargs)
+#         return view
+#
+#
+# post_detail = DetailView.as_view(Post)
 
-    def __init__(self, model):
-        self.model = model
 
-    def get_object(self, *args, **kwargs):
-        return get_object_or_404(self.model, id=kwargs['id'])
-
-    def get_template_name(self):
-        return '{}/{}_detail.html'.format(self.model._meta.app_label, self.model._meta.model_name)
-
-    def dispatch(self, request, *args, **kwargs):
-        return render(request, self.get_template_name(), {
-            self.model._meta.model_name: self.get_object(*args, **kwargs),
-        })
-
-    @classmethod
-    def as_view(cls, model):
-        def view(request, *args, **kwargs):
-            self = cls(model)
-            return self.dispatch(request, *args, **kwargs)
-        return view
-
-
-post_detail = DetailView.as_view(Post)
+# STEP 4. django 기본 제공 DetailView CBV 쓰기
+post_detail = DetailView.as_view(model=Post, pk_url_kwarg='id')
 
 
 def post_new(request):
