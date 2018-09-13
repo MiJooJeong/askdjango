16b2c41a0fa50e51c43872635f4cd70d96ab9dc8
MiJooJeong <uuuio1224@gmail.com>
2018-09-13 16:53:30 +0900

    CBV : ListView 적용

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 from . import views_cbv
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^detail/(?P<id>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^$', views_cbv.post_list, name='post_list'),
+    url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 
     url(r'^new/$', views.post_new, name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
