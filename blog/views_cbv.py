09f82448ea7aa0b6be9562224a8bca748f931eb3
MiJooJeong <uuuio1224@gmail.com>
2018-09-13 17:13:52 +0900

    CBV : CreateView/UpdateView 구현

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -2,26 +2,15 @@ from django import forms
 from django.views.generic import CreateView
 from django.views.generic import DetailView
 from django.views.generic import ListView
+from django.views.generic import UpdateView
 
 from .models import Post
 
-post_list = ListView.as_view(model=Post, paginate_by=10)
 
+post_list = ListView.as_view(model=Post, paginate_by=10)
 
 post_detail = DetailView.as_view(model=Post)
 
+post_new = CreateView.as_view(model=Post)
 
-# blog/forms.py
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        fields = '__all__'
-
-
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-    # success_url = '/...'
-
-
-post_new = PostCreateView.as_view()
+post_edit = UpdateView.as_view(model=Post, fields='__all__')
