5d00d28d5e78cd7a1f1fba8620c25186372892ed
MiJooJeong <uuuio1224@gmail.com>
2018-09-13 17:29:06 +0900

    CBV : DeleteView 구현 - reverse_lazy 적용

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
 from django.views.generic import DetailView
@@ -15,4 +16,4 @@ post_new = CreateView.as_view(model=Post)
 
 post_edit = UpdateView.as_view(model=Post, fields='__all__')
 
-post_delete = DeleteView.as_view(model=Post, success_url='/blog/')
+post_delete = DeleteView.as_view(model=Post, success_url=reverse_lazy('blog:post_list'))
