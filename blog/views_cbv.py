1c14ae3db1455ca551ed4316d1a1b216e0a823f3
MiJooJeong <uuuio1224@gmail.com>
2018-10-02 11:44:43 +0900

    prefetch_related : after 샘플 (CBV버전)

--- blog/views_cbv.py
+++ blog/views_cbv.py
@@ -1,4 +1,3 @@
-from django import forms
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import CreateView
 from django.views.generic import DeleteView
@@ -8,7 +7,14 @@ from django.views.generic import UpdateView
 
 from .models import Post
 
-post_list = ListView.as_view(model=Post, paginate_by=10)
+
+class PostListView(ListView):
+    model = Post
+    queryset = Post.objects.all().prefetch_related('tag_set', 'comments_set')
+    paginate_by = 10
+    
+
+post_list = PostListView.as_view()
 
 post_detail = DetailView.as_view(model=Post)
 
